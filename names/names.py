740393a68ceeec9ca2588460dec8d88e5a4d98d5
CarlosETurcios <turcios50@gmail.com>
2020-09-25 20:31:09 -0700

    Got runt time down to by over one minute

--- names/names.py
+++ names/names.py
@@ -18,9 +18,59 @@ for name_1 in names_1:
         if name_1 == name_2:
             duplicates.append(name_1)
 
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+
+        if value < self.value:
+
+            if self.left is None:
+
+                self.left = BSTNode(value)
+            else:
+
+                self.left.insert(value)
+
+        else:
+
+            if self.right is None:
+
+                self.right = BSTNode(value)
+
+            else:
+
+                self.right.insert(value)
+
+    def contains(self, target):
+        if target < self.value:
+            if self.left is None:
+                return False
+            return self.left.contains(target)
+        elif target > self.value:
+            if self.right is None:
+                return False
+            return self.right.contains(target)
+        else:
+            return self.value
+
+
+lList = BSTNode('')
+for name in name_1:
+    lList.insert(name)
+for name in name_2:
+    lList.insert(name)
+    if lList.contains(name):
+        duplicates.append(name)
+
+
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
